29c71c5347255692850140170343c6639e12849d
biruk_anley <birukanley2022@gmail.com>
2022-11-28 00:39:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// Namespaces
+namespace traversorypractice{
+    //main class
+    class Program{
+        //entry point method
+        static void Main (string[] args){
+            //start here
+            Console.Write("Hello, World!");
+        }
+    }
+}
+
+
